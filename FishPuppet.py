cd0beff5064c39d9cb3142b3a8fb1c15904ddba7
Jaime Nufio <jaimeenufio@gmail.com>
2017-01-19 04:33:04 -0500

    Working on keyboard commands

--- FishPuppet.py
+++ FishPuppet.py
@@ -3,28 +3,32 @@ import MotorControl as control;
 import pygame
 import time
 
-
-io.setwarnings(False);
+pygame.init();
+pygame.display.set_mode((1,1));
 
 motor1 = [17,27];
 motor2 = [5,6];
 motor3 = [23,24];
 motors = [motor1,motor2,motor3];
 
-#pygame.init()
-#pygame.display.set_mode((1,1))#screensize
-
 control.initMotor(motors);
+
 
 for motor in motors:
 	control.motorForward(motor);
 
-time.sleep(5);
+time.sleep(1);
 
 for motor in motors:
 	control.motorBackward(motor);
+time.sleep(1);
 
+control.end(motors);
 
-for motor in motors:
-	control.motorOff(motor);
+while True:
+	events = pygame.event.get()
+	for event in events:
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_q:
+					control.motorForward(motor1)
 
--- MotorControl.py
+++ MotorControl.py
@@ -9,16 +9,20 @@ def initMotor(motorset):
     io.setwarnings(False);
     io.setmode(io.BCM);
     for motor in motorset:
-    	io.setup(motor[0],io.OUT)
+    	io.setup(motor[1],io.OUT)
     	io.setup(motor[0],io.OUT)
 
     print("motors Init'd")
 	
 def motorForward(motor):
+
+    io.setmode(io.BCM);
     io.output(motor[0],0)
     io.output(motor[1],1)
 
 def motorBackward(motor):
+   
+    io.setmode(io.BCM);
     io.output(motor[0],1)
     io.output(motor[1],0)
 
@@ -30,3 +34,4 @@ def end(motors):
     for motor in motors:
         motorOff(motor)
     io.cleanup()
+    print("done!");
