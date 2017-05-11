99961c10bcde96fa05db35bb9baaca7996eac9de
Jaime Nufio <jaimeenufio@gmail.com>
2017-05-11 11:00:54 -0400

    problems with motor init

--- FishPuppet.py
+++ FishPuppet.py
@@ -1,4 +1,15 @@
 import RPi.GPIO as io;
+import MotorControl as control
+import pygame
+import time
+import pyttsx
+"""
+engine = pyttsx.init()
+
+def speak(words):
+	engine.say(words)
+	engine.runAndWait();
+"""	
 import MotorControl as control;
 import pygame
 import time
@@ -16,7 +27,7 @@ control.initMotor(motors);
 for motor in motors:
 	control.motorForward(motor);
 
-time.sleep(1);
+time.sleep(5);
 
 for motor in motors:
 	control.motorBackward(motor);
@@ -24,6 +35,47 @@ time.sleep(1);
 
 control.end(motors);
 
+active = True
+
+while active:
+	events = pygame.event.get()
+	key = pygame.key.get_pressed()
+	for event in events:
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_w:
+				control.motorForward(motor1);
+				print("Up") 
+			if event.key == pygame.K_q:
+				control.motorBackward(motor1);
+				print("Down")
+			if event.key == pygame.K_e:
+				control.motorOff(motor1);
+			if event.key == pygame.K_a:
+				control.motorForward(motor2);
+				print("Up") 
+			if event.key == pygame.K_s:
+				control.motorBackward(motor2);
+				print("Down")
+			if event.key == pygame.K_d:
+				control.motorOff(motor2);
+			if event.key == pygame.K_z:
+				control.motorForward(motor3);
+				print("Up") 
+			if event.key == pygame.K_x:
+				control.motorBackward(motor3);
+				print("Down")
+			if event.key == pygame.K_c:
+				control.motorOff(motor3);
+			if event.key == pygame.K_r:
+				control.end(motors);
+				active = False;
+				print("End.")
+				io.cleanup()
+			if event.key == pygame.K_f:
+				control.motorForward(motor2)
+				speak("Hello")
+				control.motorBackward(motor2)
+				control.motorOff(motor2)
 while True:
 	events = pygame.event.get()
 	for event in events:
--- MotorControl.py
+++ MotorControl.py
@@ -4,13 +4,15 @@ import RPi.GPIO as io;
 Easier Ussage of these basic ideas for me.
 """
 
-
 def initMotor(motorset):
     io.setwarnings(False);
     io.setmode(io.BCM);
     for motor in motorset:
     	io.setup(motor[1],io.OUT)
     	io.setup(motor[0],io.OUT)
+        io.output(motor[1],0)
+        io.output(motor[0],0)
+        
 
     print("motors Init'd")
 	
@@ -19,18 +21,28 @@ def motorForward(motor):
     io.setmode(io.BCM);
     io.output(motor[0],0)
     io.output(motor[1],1)
+#    motorOff(motor)
 
 def motorBackward(motor):
    
     io.setmode(io.BCM);
     io.output(motor[0],1)
     io.output(motor[1],0)
+#    motorOff(motor)
+
+def motorOff(motor):
+    io.setmode(io.BCM)
 
 def motorOff(motor):
     io.output(motor[0],0)
     io.output(motor[1],0)
 
 def end(motors):
+    io.setmode(io.BCM)
+    for motor in motors:
+        motorOff(motor)
+#    io.cleanup()
+
     for motor in motors:
         motorOff(motor)
     io.cleanup()
